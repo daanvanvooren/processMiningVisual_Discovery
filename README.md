d04826d43688d627f30b5d8ccd4c50c90b2a1169
Van Vooren <daanvanvooren@gmail.com>
2020-02-23 17:54:28 +0100

    First flowchart plot

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # processMiningVisual
 Process Mining visual for PowerBI as part of my bachelor thesis
+
+## About
+With Process Mining you can find bottlenecks in business processes using event log data. I used the pbiviz package from Microsoft and Mermaid.js (MIT License) to render the flowchart.
+
+## Installation 
+npm install
+yarn install
+pbiviz start
--- src/visual.ts
+++ src/visual.ts
@@ -35,47 +35,94 @@ import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInst
 import VisualObjectInstance = powerbi.VisualObjectInstance;
 import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
-
+import * as mermaid from "mermaid";
 import { VisualSettings } from "./settings";
+
+// Process Activity
+export interface Activity {
+    caseId: number;
+    activityName: string;
+    timestamp: Date;
+}
+
 export class Visual implements IVisual {
+    // Attributes
     private target: HTMLElement;
-    private updateCount: number;
     private settings: VisualSettings;
-    private textNode: Text;
+    private mermaidDiv: HTMLElement
+    private activities: Array<Activity> = new Array();
 
+    // Constructor
     constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
         this.target = options.element;
-        this.updateCount = 0;
+        this.fillActivities();
+        let graphString = this.constructGraphString(this.activities);
+
         if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
+            this.plotActivities(this.target, graphString);
         }
     }
 
+    // Update
     public update(options: VisualUpdateOptions) {
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
-        }
+
     }
 
-    private static parseSettings(dataView: DataView): VisualSettings {
-        return <VisualSettings>VisualSettings.parse(dataView);
+    // Functions
+    public fillActivities() {
+        this.activities.push({
+            caseId: 1,
+            activityName: "Start",
+            timestamp: new Date("2000-01-01")
+        });
+        this.activities.push({
+            caseId: 1,
+            activityName: "Stap2",
+            timestamp: new Date("2000-01-02")
+        });
+        this.activities.push({
+            caseId: 1,
+            activityName: "Einde",
+            timestamp: new Date("2000-01-04")
+        });
+        this.activities.push({
+            caseId: 1,
+            activityName: "Stap3",
+            timestamp: new Date("2000-01-03")
+        });
     }
 
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+    public constructGraphString(activities: Array<Activity>) {
+        var graphString: string = ""
+
+        activities.sort((a, b) => (a.timestamp > b.timestamp) ? 1 : -1)
+
+        activities.forEach(act => {
+            graphString += (act.activityName + " --> ")
+        });
+
+        graphString = graphString.slice(0, -5)
+
+        return graphString;
+    }
+
+    public plotActivities(target: HTMLElement, graphString: string) {
+        // Make mermaid div
+        this.mermaidDiv = document.createElement("div")
+        this.mermaidDiv.classList.add("graphDiv");
+        target.appendChild(this.mermaidDiv);
+
+        // Run mermaid script
+        mermaid.mermaidAPI.initialize({
+            startOnLoad: false
+        });
+
+        // Ask API to plot our graphString as SVG
+        const element: any = this.mermaidDiv;
+        const graphDefinition = "graph TB\n" + graphString;
+        console.log(graphDefinition)
+        mermaid.render("graphDiv", graphDefinition, (svgCode, bindFunctions) => {
+            element.innerHTML = svgCode;
+        });
     }
 }
